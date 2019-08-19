903ab622e02ac32f5446481c6035f31acbca8b30
976240633@qq.com <070405wkh>
2019-08-19 23:08:21 +0800

    weiekhang-dev 添加了log

--- redux-learn/src/App.js
+++ redux-learn/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    console.log("添加了log weikehang-dev")
   return (
     <div className="App">
       <header className="App-header">

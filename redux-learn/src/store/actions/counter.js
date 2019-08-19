df474f2742b175c9dae7b37b4f7afec67634fea5
976240633@qq.com <070405wkh>
2019-08-19 21:33:12 +0800

    修改

--- redux-learn/src/store/actions/counter.js
+++ redux-learn/src/store/actions/counter.js
@@ -2,7 +2,7 @@ import * as types from '../../types/action-types';
 import {push} from '../../connected-react-router';
 export default {
     add(){
-        return {type:types.ADD1};
+        return {type:types.ADD};
     },
     goto(path,state){
         return push(path,state);

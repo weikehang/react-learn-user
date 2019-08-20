9b2e4bc96178c367792fc028b3a87a8a7baf83c3
976240633@qq.com <070405wkh>
2019-08-20 08:22:03 +0800

    哈哈哈添加自己的

--- redux-learn/src/App.js
+++ redux-learn/src/App.js
@@ -9,7 +9,7 @@ function App() {
 	  <header className="App-header">
 		<img src={logo} className="App-logo" alt="logo" />
 		<p>
-		  Edit <code>src/App.js</code> and save to reload.
+		  Edit 我自己天界了东西的
 		</p>
 		<a
 		  className="App-link"
